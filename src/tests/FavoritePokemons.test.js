a3c37f388f2c82e4604f41226d63241d8c3825e3
laujuu <lauroindcontato@gmail.com>
2022-06-17 21:15:53 -0300

    Adiciona requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import App from '../App';
+
+import renderWithRouter from '../renderWithRouter';
+import userEvent from '@testing-library/user-event';
+
+describe('Requisito 01 - Componente <About.js /> ', () => {
+  test('Se exibe "No favorite pokemon found" caso vazio', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+
+  test('Se são exibidos todos os cards favoritados', () => {
+    const { container, getByRole, history } = renderWithRouter(<App />);
+
+    userEvent.click(getByRole('link', { name: /More details/i }));
+    userEvent.click(getByRole('checkbox', { name: /Pokémon favoritado?/i }));
+    history.push('/favorites');
+
+    expect(container.getElementsByClassName('favorite-pokemon').length).toBe(1);
+  });
+});
